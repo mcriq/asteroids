7ab1e7cd6ac977f7b5d99b4bbd2612387b0b3a03
mcriq <125591030+mcriq@users.noreply.github.com>
2024-12-20 13:21:49 -0600

    add sprite groups to clean up main game loop

--- main.py
+++ main.py
@@ -4,19 +4,25 @@ from player import Player
 
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         # limit framerate to 60 FPS
         dt = clock.tick(60) / 1000
