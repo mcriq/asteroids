118ce3702b954e34e1c76c4d64b1123b3966c67f
mcriq <125591030+mcriq@users.noreply.github.com>
2024-12-20 11:59:24 -0600

    add player triangle to game screen

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pygame
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         # limit framerate to 60 FPS
         dt = clock.tick(60) / 1000
