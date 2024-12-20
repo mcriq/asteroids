41a66d606d23d25b1d5cac262001f87fdea06d37
mcriq <125591030+mcriq@users.noreply.github.com>
2024-12-20 11:15:34 -0600

    limit framerate to 60 FPS

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from constants import SCREEN_WIDTH,SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        # limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
